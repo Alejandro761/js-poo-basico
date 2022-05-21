54815039626f596f88bf647a62d18c8fe72008cc
Alejandro Castaneda <alejandroxd62@gmail.com>
2022-05-20 22:35:42 -0500

    modulos en js. Clase 12

---
+++ module.mjs
@@ -0,0 +1,9 @@
+import {PlatziClass} from "./main2.mjs";
+
+const clase67 = new PlatziClass({
+    name: 'JavaScript: orientado a objetos, basado en proptotipos',
+    videoID: '19awd98dcf8n12fdz36s'
+});
+
+clase67.reproducir();
+clase67.pausar();
