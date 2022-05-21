16f6dff03eed8f8fba87e2be28725387fdfd0aa7
Alejandro Castaneda <alejandroxd62@gmail.com>
2022-05-20 21:37:21 -0500

    abstraccion. Clase9

--- main2.js
+++ main2.js
@@ -11,12 +11,40 @@ class Student{
     }
 }
 
-class learningPaths {
+class LearningPaths {
     constructor({name, courses = []}){
         this.name = name;
         this.courses = courses;
     }
 }
+
+class Course {
+    constructor({name, classes = []}){
+        this.name = name;
+        this.classes = classes;
+    }
+}
+
+class Class {
+    constructor({name, duration})
+}
+
+const cursoProgBasica = new Course({
+    name: "Curso de Programación básica",
+    classes: ''
+})
+
+const cursoHTMLCSS = new Course({
+    name: "Curso de HTML y CSS",
+    classes: ''
+})
+
+const cursoGridFlex = new Course({
+    name: "Curso de Gird y Flexbox",
+    classes: ''
+})
+
+const escuelaWeb = new LearningPaths({name: 'Escuela de Desarrollo Web', courses: [cursoProgBasica, cursoHTMLCSS, cursoGridFlex]} ) 
 
 const ale = new Student({name: 'Alejandro', username: 'ale🇲🇽', instagram: 'Alex', twitter: 'alejandro_xd62', email: 'ale@si.com'})
 console.log(ale);
